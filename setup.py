53d461f8750f7b31bddcc71810e157d30c991074
amaheshwari01 <aayan@aayan.com>
2026-04-08 18:42:13 -0400

    yet another small fix

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     },
     entry_points={
         'console_scripts': [
-            
+            'cameraPublisher = tauv_vision.cameraPublisher:main'
         ],
     },
 )
--- tauv_vision/cameraPublisher.py
+++ tauv_vision/cameraPublisher.py
@@ -43,67 +43,68 @@ class CameraPublisher(Node):
         self.pub_left.publish(msg_left)
         self.pub_right.publish(msg_right)
 
-rclpy.init()
-cam_node = CameraPublisher()
+def main(args=None):
+    rclpy.init(args=args)
+    cam_node = CameraPublisher()
 
-with dai.Pipeline() as pipeline:
-    # Define Camera Nodes
-    cam_rgb = pipeline.create(dai.node.Camera).build(dai.CameraBoardSocket.CAM_A)
-    cam_left = pipeline.create(dai.node.Camera).build(dai.CameraBoardSocket.CAM_B)
-    cam_right = pipeline.create(dai.node.Camera).build(dai.CameraBoardSocket.CAM_C)
+    with dai.Pipeline() as pipeline:
+        # Define Camera Nodes
+        cam_rgb = pipeline.create(dai.node.Camera).build(dai.CameraBoardSocket.CAM_A)
+        cam_left = pipeline.create(dai.node.Camera).build(dai.CameraBoardSocket.CAM_B)
+        cam_right = pipeline.create(dai.node.Camera).build(dai.CameraBoardSocket.CAM_C)
 
-    # Define Sync Node
-    sync = pipeline.create(dai.node.Sync)
-    sync.setSyncThreshold(timedelta(milliseconds=15))
+        # Define Sync Node
+        sync = pipeline.create(dai.node.Sync)
+        sync.setSyncThreshold(timedelta(milliseconds=15))
 
-    # We request the output from the cameras (internal) and link to Sync
-    if MAX_RES:
-        cam_rgb.requestOutput((4056,3040)).link(sync.inputs["rgb"])
-    else:
-        cam_rgb.requestOutput((1920, 1080)).link(sync.inputs["rgb"])
-    cam_left.requestOutput((1280, 800)).link(sync.inputs["left"])
-    cam_right.requestOutput((1280, 800)).link(sync.inputs["right"])
+        # We request the output from the cameras (internal) and link to Sync
+        if MAX_RES:
+            cam_rgb.requestOutput((4056,3040)).link(sync.inputs["rgb"])
+        else:
+            cam_rgb.requestOutput((1920, 1080)).link(sync.inputs["rgb"])
+        cam_left.requestOutput((1280, 800)).link(sync.inputs["left"])
+        cam_right.requestOutput((1280, 800)).link(sync.inputs["right"])
 
-    # 5. Create the Synced Output Queue
-    # We call createOutputQueue directly from the sync node's output port
-    syncQueue = sync.out.createOutputQueue()
+        # 5. Create the Synced Output Queue
+        # We call createOutputQueue directly from the sync node's output port
+        syncQueue = sync.out.createOutputQueue()
 
-    controlQueueRGB = cam_rgb.inputControl.createInputQueue()
+        controlQueueRGB = cam_rgb.inputControl.createInputQueue()
 
-    pipeline.start()
+        pipeline.start()
 
-    # Set manual focus once the pipeline is running
-    controlQueueRGB = cam_rgb.inputControl.createInputQueue()
-    controlQueueLeft = cam_left.inputControl.createInputQueue()
-    controlQueueRight = cam_right.inputControl.createInputQueue()
+        # Set manual focus once the pipeline is running
+        controlQueueRGB = cam_rgb.inputControl.createInputQueue()
+        controlQueueLeft = cam_left.inputControl.createInputQueue()
+        controlQueueRight = cam_right.inputControl.createInputQueue()
 
-    ctrl = dai.CameraControl()
-    ctrl.setManualFocus(130)
-    ctrl.setManualExposure(20000, 1400)
-    ctrl.setManualWhiteBalance(7000)
+        ctrl = dai.CameraControl()
+        ctrl.setManualFocus(130)
+        ctrl.setManualExposure(20000, 1400)
+        ctrl.setManualWhiteBalance(7000)
 
-    controlQueueRGB.send(ctrl)
-    controlQueueLeft.send(ctrl)
-    controlQueueRight.send(ctrl)
+        controlQueueRGB.send(ctrl)
+        controlQueueLeft.send(ctrl)
+        controlQueueRight.send(ctrl)
 
-    # and, go!
-    pipeline.start()
+        # and, go!
+        pipeline.start()
 
 
-    while pipeline.isRunning():
-        # Get the synchronized group
-        msg_group = syncQueue.get()
-        
-        # Access frames by the keys used in the links
-        f_rgb = msg_group["rgb"].getCvFrame()
-        f_left = msg_group["left"].getCvFrame()
-        f_right = msg_group["right"].getCvFrame()
+        while pipeline.isRunning():
+            # Get the synchronized group
+            msg_group = syncQueue.get()
+            
+            # Access frames by the keys used in the links
+            f_rgb = msg_group["rgb"].getCvFrame()
+            f_left = msg_group["left"].getCvFrame()
+            f_right = msg_group["right"].getCvFrame()
 
-        # Publish!
-        cam_node.publish_frames(f_rgb, f_left, f_right)
-        
-        # Spin once to handle any callbacks if necessary
-        rclpy.spin_once(cam_node, timeout_sec=0)
+            # Publish!
+            cam_node.publish_frames(f_rgb, f_left, f_right)
+            
+            # Spin once to handle any callbacks if necessary
+            rclpy.spin_once(cam_node, timeout_sec=0)
 
-cam_node.destroy_node()
-rclpy.shutdown()
+    cam_node.destroy_node()
+    rclpy.shutdown()
